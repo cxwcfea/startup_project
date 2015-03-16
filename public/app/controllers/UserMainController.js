b460053bf1bb8b31e3fb4244c1d3bb64cd294a3f
cxwcfea <cxwcfea@163.com>
2015-03-16 19:25:43 +0800

    total capital

--- public/app/controllers/UserMainController.js
+++ public/app/controllers/UserMainController.js
@@ -3,6 +3,8 @@ angular.module('myApp').controller('UserMainController', ['gbIdentity', function
     var vm = this;
     vm.user = gbIdentity.currentUser;
 
+    vm.user_total_capital = (vm.user.finance.balance + vm.user.finance.deposit + vm.user.finance.freeze_capital).toFixed(2);
+
     vm.displayName = function() {
         if (vm.user.profile.name) {
             return vm.user.profile.name;
@@ -13,4 +15,5 @@ angular.module('myApp').controller('UserMainController', ['gbIdentity', function
     vm.registerDate = function() {
         return moment(vm.user.registerAt).format('ll');
     }
+
 }]);
