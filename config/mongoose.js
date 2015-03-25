a3cdb24bff574511237423de3f2ec60fbc9b9023
cxwcfea <cxwcfea@163.com>
2015-03-25 10:21:08 +0800

    login

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,16 +8,33 @@ function createDefaultUsers() {
         if (collection.length === 0) {
             User.create({mobile: 13439695920, password: 'xxxxxx', registered:true, profile: {name:'cxwcfea'}, roles: ['admin']}, function (err, user) {
                 if (err) console.log('create & find user err:' + err);
+                /*
                 if (user) {
                     createOrderDefaultUsers(user._id);
                 }
+                */
             });
 
+            /*
             User.create({mobile: 13121909306, password: 'xxxxxx', roles: ['admin']});
             User.create({mobile: 13810655219, password: 'xxxxxx', roles: ['admin']});
             User.create({mobile: 18911535534, password: 'xxxxxx', roles: ['admin']});
             User.create({mobile: 18612921262, password: 'xxxxxx', roles: ['admin']});
+            */
 
+            /*
+            User.create({mobile: 11111111111, password: '123456', roles: ['admin']});
+            User.create({mobile: 22222222222, password: '123456', roles: ['admin']});
+            User.create({mobile: 33333333333, password: '123456', roles: ['admin']});
+            User.create({mobile: 44444444444, password: '123456', roles: ['admin']});
+            User.create({mobile: 55555555555, password: '123456', roles: ['admin']});
+            User.create({mobile: 66666666666, password: '123456', roles: ['admin']});
+            User.create({mobile: 77777777777, password: '123456', roles: ['admin']});
+            User.create({mobile: 88888888888, password: '123456', roles: ['admin']});
+            User.create({mobile: 99999999999, password: '123456', roles: ['admin']});
+            */
+
+            /*
             Homas.create({account: '123450', password: '111111'});
             Homas.create({account: '123451', password: '111111'});
             Homas.create({account: '123452', password: '111111'});
@@ -38,6 +55,7 @@ function createDefaultUsers() {
             Homas.create({account: '123467', password: '111111'});
             Homas.create({account: '123468', password: '111111'});
             Homas.create({account: '123469', password: '111111'});
+            */
         }
     });
 }
