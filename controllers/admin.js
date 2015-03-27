80f0f826a5e799c104e3f56fb1c13bdbacaa0f75
cxwcfea <cxwcfea@163.com>
2015-03-27 18:34:54 +0800

    fix typo

--- controllers/admin.js
+++ controllers/admin.js
@@ -507,8 +507,8 @@ function autoFetchPendingApplies(req, res) {
                 "deposit": apply.isTrial ? 1 : apply.deposit,
                 "lever":apply.isTrial ? 2000 : 9,
                 "amount":apply.isTrial ? 2000 : apply.amount-apply.deposit,
-                "margin_call":apply.isTrial ?  : config.warnFactor * apply.amount,
-                "close":apply.isTrial ? 0 : config.sellFactor * apply.amount
+                "margin_call":apply.isTrial ? 1800 : config.warnFactor * apply.amount,
+                "close":apply.isTrial ? 1600 : config.sellFactor * apply.amount
             }
         });
         res.send(ret);
