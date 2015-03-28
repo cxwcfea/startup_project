b118b5f2b82cb56d768e4e4e9e4763b17723acd9
cxwcfea <cxwcfea@163.com>
2015-03-28 16:14:06 +0800

    make card id as string

--- models/Card.js
+++ models/Card.js
@@ -4,7 +4,7 @@ var cardSchema = mongoose.Schema({
     userID: {type:String, required:'{PATH} is required!'},
     bankID: {type:Number, required:'{PATH} is required!'},
     bankName: {type:String, required:'{PATH} is required!'},
-    cardID: {type:Number, required:'{PATH} is required!'},
+    cardID: {type:String, required:'{PATH} is required!'},
     userName: {type:String, required:'{PATH} is required!'}
 });
 
