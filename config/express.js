c60671e04d2a3bd1c5f4bfe2d44fcfc37e55a23c
cxwcfea <cxwcfea@163.com>
2015-03-29 11:23:26 +0800

    improvement

--- config/express.js
+++ config/express.js
@@ -53,7 +53,7 @@ module.exports = function(app, config) {
     app.use(expressValidator());
     app.use(session({
         cookie: {
-            maxAge: 24 * 60 * 60 * 1000
+            maxAge: 2 * 60 * 60 * 1000
         },
         store: sessionStore,
         secret: 'the secret key',
