56b2897d4df56404d876e365cc64b14f29049dfc
cxwcfea <cxwcfea@163.com>
2015-04-08 14:06:06 +0800

    show history capital in the main page

--- controllers/home.js
+++ controllers/home.js
@@ -13,7 +13,7 @@ function home(req, res, next) {
         return;
     }
     if (!req.session.statistic || req.session.statistic.expires < Date.now()) {
-        User.aggregate([{$match:{registered:true}}, {$group: {_id: null, count: {$sum: 1}, profit: { $sum: '$finance.profit'}, capital: { $sum: '$finance.total_capital' } }}], function(err, statistic) {
+        User.aggregate([{$match:{registered:true}}, {$group: {_id: null, count: {$sum: 1}, profit: { $sum: '$finance.profit'}, capital: { $sum: '$finance.history_capital' } }}], function(err, statistic) {
             if (err || !statistic) {
                 logger.warn('error when fetch total user count:' + err.toString());
                 statistic = [{
