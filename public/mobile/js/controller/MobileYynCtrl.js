b25d7843837eca03113b8fe5debf5bc69d89f5e1
cxwcfea <cxwcfea@163.com>
2015-04-09 15:52:14 +0800

    yyn in prograssing

--- public/mobile/js/controller/MobileYynCtrl.js
+++ public/mobile/js/controller/MobileYynCtrl.js
@@ -5,23 +5,30 @@ angular.module('mobileApp').controller('MobileYynCtrl', ['$scope', '$window', fu
     vm.parameterList = [
         {
             name: '2倍',
+            interest: 1.3,
             value: 2
         },
         {
             name: '3倍',
+            interest: 1.4,
             value: 3
         },
         {
             name: '4倍',
+            interest: 1.5,
             value: 4
         },
         {
             name: '5倍',
+            interest: 1.6,
             value: 5
         },
         {
             name: '6倍',
+            interest: 1.7,
             value: 6
         }
     ];
+
+    vm.selectedValue = vm.parameterList[0];
 }]);
