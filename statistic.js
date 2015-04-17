d8cb7a0ee669f3f1ccb1f446f4bb1abf7b5579d4
cxwcfea <cxwcfea@163.com>
2015-04-17 17:03:45 +0800

    script to find some data

--- statistic.js
+++ statistic.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
     Apply = require('./models/Apply'),
+    Order = require('./models/Order'),
 	User = require('./models/User'),
     config = require('./config/config')['production'];
 
@@ -22,6 +23,27 @@ var getPayUserNum = function() {
     });
 };
 
+var withDrawOrder = function() {
+    var today = moment().startOf('day');
+    /*
+    Order.find({ $and: [{approvedAt:{$gte:today}},  {dealType:2}] }, function(err, order) {
+        if (err || !order) {
+            console.log(err.toString());
+        } else {
+
+        }
+    })
+    */
+    var yestoday = today.subtract(1, 'days');
+    Apply.find({$and:[{isTrial:true}, {endTime:{$lte:today}}, {endTime:{$gte:yestoday}}]}, function(err, applies) {
+        for (var i = 0; i < applies.length; ++i) {
+            User.findById(applies[i].userID, function(err, user) {
+                console.log(user);
+            });
+        }
+    });
+};
+
 var options = {};
 mongoose.connect(config.db, options);
 var db = mongoose.connection;
@@ -29,4 +51,5 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
     console.log('goldenbull db opened');
 });
-getPayUserNum();
+//getPayUserNum();
+withDrawOrder();
