231cd6197d4808363ca1e6b54047c5243c4e6b08
cxwcfea <cxwcfea@163.com>
2015-04-22 17:47:23 +0800

    update statistic

--- statistic/statistic.js
+++ statistic/statistic.js
@@ -2,25 +2,17 @@ var mongoose = require('mongoose'),
     fs = require('fs'),
     moment = require('moment'),
     _ = require('lodash'),
+    async = require('async'),
     Apply = require('../models/Apply'),
     Card = require('../models/Card'),
     Order = require('../models/Order'),
     User = require('../models/User'),
     config = require('../config/config')['production'];
 
-var dailyFreeApplyData = function(db) {
+var dailyFreeApplyData = function(startTime, endTime, callback) {
     console.log('dailyFreeApplyData');
-    var endTime = moment();
-    endTime.hour(15);
-    endTime.minute(00);
-    endTime.second(00);
 
-    var startTime = endTime.clone();
-    startTime = startTime.subtract(1, 'days');
-    startTime = startTime.toDate();
-    endTime = endTime.toDate();
-
-    Apply.find({$and:[{closeAt:{$lte:endTime}}, {closeAt:{$gte:startTime}}, {isTrial:true}]}, function(err, applies) {
+    Apply.find({$and:[{closeAt:{$lte:endTime}}, {closeAt:{$gte:startTime}}, {isTrial:true}, {status:3}]}, function(err, applies) {
         if (err) {
             console.log(err.toString());
             return;
@@ -32,14 +24,41 @@ var dailyFreeApplyData = function(db) {
         });
         var data = 'userID, userMobile, serialID, amount, deposit, period, status, applyAt, closeAt, isTrial, autoPostpone, lever, warnValue, sellValue, startTime, endTime, account, profit, type, interestRate, serviceCharge\n';
         applies.forEach(function (apply) {
-            data = apply.userID + ', ' + apply.userMobile + ', ' + apply.serialID + ', ' + apply.amount.toFixed(2) + ', ' + apply.deposit.toFixed(2) + ', '
+            data += apply.userID + ', ' + apply.userMobile + ', ' + apply.serialID + ', ' + apply.amount.toFixed(2) + ', ' + apply.deposit.toFixed(2) + ', '
                 + apply.period + ', ' + apply.status + ', ' + apply.applyAt + ', ' + apply.closeAt + ', ' + apply.isTrial + ', ' + apply.autoPostpone + ', '
                 + apply.lever + ', ' + apply.warnValue + ', ' + apply.sellValue + ', ' + apply.startTime + ', ' + apply.endTime + ', ' + apply.account + ', '
                 + apply.profit + ', ' + apply.type + ', ' + apply.interestRate + ', ' + apply.serviceCharge + '\n';
             fileWriteStream.write(data);
         });
         fileWriteStream.end();
-        db.close();
+        callback(null);
+    });
+};
+
+var dailyPayApplyData = function(startTime, endTime, callback) {
+    console.log('dailyPayApplyData');
+
+    Apply.find({$and:[{closeAt:{$lte:endTime}}, {closeAt:{$gte:startTime}}, {isTrial:false}, {status:3}]}, function(err, applies) {
+        if (err) {
+            console.log(err.toString());
+            callback(err);
+            return;
+        }
+        var options = { encoding: 'utf8', flag: 'w' };
+        var fileWriteStream = fs.createWriteStream("dailyPayApplyData-" + moment().format("YYYY-MM-DD") + ".txt",  options);
+        fileWriteStream.on("close", function() {
+            console.log("File Closed.");
+        });
+        var data = 'userID, userMobile, serialID, amount, deposit, period, status, applyAt, closeAt, isTrial, autoPostpone, lever, warnValue, sellValue, startTime, endTime, account, profit, type, interestRate, serviceCharge\n';
+        applies.forEach(function (apply) {
+            data += apply.userID + ', ' + apply.userMobile + ', ' + apply.serialID + ', ' + apply.amount.toFixed(2) + ', ' + apply.deposit.toFixed(2) + ', '
+            + apply.period + ', ' + apply.status + ', ' + apply.applyAt + ', ' + apply.closeAt + ', ' + apply.isTrial + ', ' + apply.autoPostpone + ', '
+            + apply.lever + ', ' + apply.warnValue + ', ' + apply.sellValue + ', ' + apply.startTime + ', ' + apply.endTime + ', ' + apply.account + ', '
+            + apply.profit + ', ' + apply.type + ', ' + apply.interestRate + ', ' + apply.serviceCharge + '\n';
+            fileWriteStream.write(data);
+        });
+        fileWriteStream.end();
+        callback(null);
     });
 };
 
@@ -50,6 +69,33 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
     console.log('goldenbull db opened');
-    dailyFreeApplyData(db);
+    var endTime = moment();
+    endTime.hour(15);
+    endTime.minute(00);
+    endTime.second(00);
+
+    var startTime = endTime.clone();
+    startTime = startTime.subtract(1, 'days');
+    startTime = startTime.toDate();
+    endTime = endTime.toDate();
+    async.series(
+        [
+            function(callback){
+                dailyFreeApplyData(startTime, endTime, function(err) {
+                    callback(err);
+                });
+            },
+            function(callback){
+                dailyPayApplyData(startTime, endTime, function(err) {
+                    callback(err);
+                });
+            }
+        ],
+        function(err){
+            if (err) {
+                console.log(err.toString());
+            }
+            db.close();
+        });
 });
 
