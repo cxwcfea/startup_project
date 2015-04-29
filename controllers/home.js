00c3ac66fab7f7df6697a7148ddbe2f0d600a95f
cxwcfea <cxwcfea@163.com>
2015-04-29 10:32:27 +0800

    statistic data in main page change

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 var User = require('../models/User'),
     Apply = require('../models/Apply'),
+    DailyData = require('../models/DailyData'),
     util = require('../lib/util'),
     useragent = require('useragent'),
     log4js = require('log4js'),
@@ -12,7 +13,7 @@ function home(req, res, next) {
         return;
     }
     if (!req.session.statistic || req.session.statistic.expires < Date.now()) {
-        User.aggregate([{$match:{registered:true}}, {$group: {_id: null, count: {$sum: 1}, profit: { $sum: '$finance.profit'}, capital: { $sum: '$finance.history_capital' }, current_capital: { $sum: '$finance.total_capital' } }}], function(err, statistic) {
+        User.aggregate([{$match:{registered:true}}, {$group: {_id: null, count: {$sum: 1}, profit: { $sum: '$finance.profit'}, capital: { $sum: '$finance.history_capital' } }}], function(err, statistic) {
             if (err || !statistic) {
                 logger.warn('error when fetch total user count:' + err.toString());
                 statistic = [{
@@ -21,59 +22,83 @@ function home(req, res, next) {
                     profit: 4000000
                 }];
             }
-            Apply
-                .find({})
-                .sort({ _id: -1 })
-                .limit(5)
-                .exec(function(err, applies) {
-                    var theApplies;
-                    if (!applies) {
-                        theApplies = [
-                            {
-                                userMobile: '134******20',
-                                amount: 50000
-                            },
-                            {
-                                userMobile: '131******06',
-                                amount: 100000
-                            },
-                            {
-                                userMobile: '138******19',
-                                amount: 200000
-                            },
-                            {
-                                userMobile: '159******65',
-                                amount: 2000
-                            },
-                            {
-                                userMobile: '135******14',
-                                amount: 250000
-                            }
-                        ];
-                    } else {
-                        theApplies = applies.map(function(a) {
-                            return {
-                                userMobile: util.mobileDisplay(a.userMobile),
-                                amount: a.amount > 20000 ? a.amount : a.amount * 10
-                            }
-                        });
+            DailyData.find({}, function(err, dailyData) {
+                if (err) {
+                    logger.warn('error when fetch daily data:' + err.toString());
+                    statistic[0].dailyAmount = 10000000;
+                } else {
+                    var amount = 0;
+                    for (var i = 0; i < dailyData.length; ++i) {
+                        amount += dailyData[i].applyAmount;
                     }
-                    req.session.statistic = {
-                        user_count: statistic[0].count + 7000,
-                        total_capital: statistic[0].capital + 200000000 + statistic[0].current_capital,
-                        total_profit: (statistic[0].profit + 4000000).toFixed(0),
-                        show_applies: theApplies,
-                        expires: Date.now() + 3600000 * 1
-                    };
+                    statistic[0].dailyAmount = amount;
+                }
+                Apply
+                    .find({})
+                    .sort({ _id: -1 })
+                    .limit(8)
+                    .exec(function(err, applies) {
+                        var theApplies;
+                        if (!applies) {
+                            theApplies = [
+                                {
+                                    userMobile: '134******20',
+                                    amount: 50000
+                                },
+                                {
+                                    userMobile: '131******06',
+                                    amount: 100000
+                                },
+                                {
+                                    userMobile: '138******19',
+                                    amount: 200000
+                                },
+                                {
+                                    userMobile: '159******65',
+                                    amount: 2000
+                                },
+                                {
+                                    userMobile: '138******65',
+                                    amount: 23000
+                                },
+                                {
+                                    userMobile: '139******35',
+                                    amount: 62000
+                                },
+                                {
+                                    userMobile: '137******68',
+                                    amount: 7800
+                                },
+                                {
+                                    userMobile: '135******14',
+                                    amount: 250000
+                                }
+                            ];
+                        } else {
+                            theApplies = applies.map(function(a) {
+                                return {
+                                    userMobile: util.mobileDisplay(a.userMobile),
+                                    amount: a.amount
+                                }
+                            });
+                        }
+                        req.session.statistic = {
+                            user_count: statistic[0].count + 7000,
+                            total_capital: statistic[0].capital + 200000000 + statistic[0].dailyAmount,
+                            total_profit: (statistic[0].profit + 4000000).toFixed(0),
+                            show_applies: theApplies,
+                            expires: Date.now() + 3600000 * 1
+                        };
 
-                    res.locals.main_menu = true;
-                    res.render('home', {
-                        user_count: req.session.statistic.user_count,
-                        total_capital: util.formatDisplayNum(req.session.statistic.total_capital),
-                        total_profit: req.session.statistic.total_profit,
-                        apply_infos: req.session.statistic.show_applies
+                        res.locals.main_menu = true;
+                        res.render('home', {
+                            user_count: req.session.statistic.user_count,
+                            total_capital: util.formatDisplayNum(req.session.statistic.total_capital),
+                            total_profit: req.session.statistic.total_profit,
+                            apply_infos: req.session.statistic.show_applies
+                        });
                     });
-                });
+            });
         });
     } else {
         res.locals.main_menu = true;
