f212213d1f3a18609c065995dd1c825811069317
cxwcfea <cxwcfea@163.com>
2015-05-05 11:04:00 +0800

    improve postpone apply stability

--- controllers/apply.js
+++ controllers/apply.js
@@ -8,6 +8,7 @@ var Apply = require('../models/Apply'),
     logger = log4js.getLogger('apply'),
     _ = require('lodash'),
     async = require('async'),
+    sms = require('./sms'),
     util = require('../lib/util');
 
 exports.getYYnPage = function(req, res, next) {
@@ -230,7 +231,12 @@ exports.postApplyPostpone = function(req, res, next) {
                         user.save(function(err) {
                             var content = 'user:' + order.userMobile + ' account:' + apply.account + ' period:' + period;
                             util.sendEmail('op@niujinwang.com,intern@niujinwang.com', '配资延期', content, function(err) {
-                                logger.debug('error when send postpone email');
+                                logger.debug('error when send postpone email apply:' + apply.serialID + ' account:' + apply.account + ' ' + err.toString());
+                                sms.sendSMS('13439695920', '', 'apply postpone ' + content, function (result) {
+                                    if (result.error) {
+                                        logger.debug('sms also send error when postpone apply:' + apply.serialID);
+                                    }
+                                });
                             });
                             callback(err, order, true);
                         });
