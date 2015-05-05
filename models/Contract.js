82a7bb1e46a664258e3468161bf5dc8531007fc0
cxwcfea <cxwcfea@163.com>
2015-05-05 13:00:54 +0800

    p2p continue

--- models/Contract.js
+++ models/Contract.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 
 var contractSchema = mongoose.Schema({
     applySerialID: {type:String, required:'{PATH} is required!'},
+    userMobile: {type:String, required:'{PATH} is required!'},
     amount: {type:Number, required:'{PATH} is required!'},
-    status: {type:Number, default: 1}, // 0 wait for confirm, 1 pay success, 2 not pay, 3 otherwise
+    status: {type:Number, default: 1}, // 0 reserve, 1 processing, 2 finished
     investors: [String],
     startTime: Date,
     endTime: Date
