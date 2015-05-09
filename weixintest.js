5f92a02763b56f1a6ad34863349d32185900c560
cxwcfea <cxwcfea@163.com>
2015-05-09 14:56:36 +0800

    add weixin menu

--- weixintest.js
+++ weixintest.js
@@ -52,11 +52,9 @@ module.exports.checkSignature = function(req, res) {
     } else {
         res.send("error data");
     }
-
-    sendMenu();
 };
 
-function getManagerMenu() {
+function test() {
     var StockButton = new ComplexButton('配资炒股');
     var AppDownLoadButton = new ComplexButton('下载软件');
     var MyAccountButton = new ComplexButton('我的账户');
@@ -89,31 +87,70 @@ function getManagerMenu() {
         ]
     };
     return JSON.stringify(obj);
+}
+console.log(test());
+
+module.exports.getManagerMenu = function(req, res) {
+    var StockButton = new ComplexButton('配资炒股');
+    var AppDownLoadButton = new ComplexButton('下载软件');
+    var MyAccountButton = new ComplexButton('我的账户');
+
+    var ButtonFreeTry = new Button('免费体验','http://m.niujinwang.com/free_apply');
+    var ButtonDaysNiu = new Button('天天牛','http://m.niujinwang.com/mobile/#/ttn');
+
+    StockButton.sub_button[0] = ButtonFreeTry;
+    StockButton.sub_button[1] = ButtonDaysNiu;
+
+    var ButtonIPhoneApp = new Button('iPhone版','https://itunes.apple.com/cn/app/homs-yong-jin-ban/id950510961?mt=8');
+    var ButtonAndroidApp = new Button('Android版','http://www.ihoms.com/file/xunlei/downloadxunlei/HOMS_YONGJINBAN_android.apk?fileCode=38&sourceType=4');
+
+    AppDownLoadButton.sub_button[0] = ButtonIPhoneApp;
+    AppDownLoadButton.sub_button[1] = ButtonAndroidApp;
+
+    var ButtonRegister= new Button('注册','http://www.niujinwang.com/mobile/#/signup');
+    var ButtonLogin = new Button('登录','http://www.niujinwang.com/mobile/#/login');
+    var ButtonUser = new Button('个人中心','http://www.niujinwang.com/mobile/#/user');
+
+    MyAccountButton.sub_button[0] = ButtonRegister;
+    MyAccountButton.sub_button[1] = ButtonLogin;
+    MyAccountButton.sub_button[2] = ButtonUser;
+
+    var obj = {
+        button: [
+            StockButton,
+            AppDownLoadButton,
+            MyAccountButton
+        ]
+    };
+    return Json.stringify(obj);
 };
 
-function getToken() {
-    if (!global.weixin_token || global.weixin_token.expires > Date.now()) {
-        https.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx182b3688b9b060f7&secret=13ea8e4782476ccdf2d58bbbb1634513', function(res) {
-            console.log(res);
-            global.weixin_token = {
-                token: res.access_token,
-                expires: Date.now() + 7200000 // 2 hour
-            }
-        });
-    }
+module.exports.getToken = function() {
+    https.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx182b3688b9b060f7&secret=13ea8e4782476ccdf2d58bbbb1634513', function(res) {
+        console.log(res);
+        global.weixin_token = {
+            token: res.access_token,
+            expires: Date.now() + 7200000 // 2 hour
+        }
+    });
 };
 
-function sendMenu() {
-    getToken();
-    var postData = getManagerMenu();
+module.exports.sendMenue = function() {
+    var postData = querystring.stringify({
+        'account' : 'VIP_njw',
+        'pswd' : 'Tch123456',
+        'mobile' : mobile_num,
+        'msg' : message_content,
+        'needstatus' : false
+    });
 
     var options = {
-        hostname: 'https://api.weixin.qq.com',
+        hostname: '222.73.117.158',
         port: 80,
-        path:'/cgi-bin/menu/create?access_token=' + global.weixin_token.token,
+        path:'/msg/HttpBatchSendSM',
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': postData.length
         }
     };
@@ -124,15 +161,8 @@ function sendMenu() {
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             console.log('BODY: ' + chunk);
+            cb({error_code:0});
         });
     });
-
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-    });
-
-    // write data to request body
-    req.write(postData);
-    req.end();
 };
 
