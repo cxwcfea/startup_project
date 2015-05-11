7b5af8b4ac9ac2c8149751989281d7896a1e46d0
cxwcfea <cxwcfea@163.com>
2015-05-11 14:15:57 +0800

    fix bug when apply postpone

--- controllers/apply.js
+++ controllers/apply.js
@@ -233,12 +233,12 @@ exports.postApplyPostpone = function(req, res, next) {
                             util.sendEmail('op@niujinwang.com,intern@niujinwang.com', '配资延期', content, function(err) {
                                 if (err) {
                                     logger.debug('error when send postpone email apply:' + apply.serialID + ' account:' + apply.account + ' ' + err.toString());
+                                    sms.sendSMS('13439695920', '', 'apply postpone ' + content, function (result) {
+                                        if (result.error) {
+                                            logger.debug('sms also send error when postpone apply:' + apply.serialID);
+                                        }
+                                    });
                                 }
-                                sms.sendSMS('13439695920', '', 'apply postpone ' + content, function (result) {
-                                    if (result.error) {
-                                        logger.debug('sms also send error when postpone apply:' + apply.serialID);
-                                    }
-                                });
                             });
                             callback(err, order, true);
                         });
