1dcda819e8d5f405f5ff8ce91e06923ed2734dea
cxwcfea <cxwcfea@163.com>
2015-07-06 21:03:34 +0800

    ppj continue

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -1,4 +1,52 @@
 'use strict';
-angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window', function($scope, $window) {
+angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window', '$modal', function($scope, $window, $modal) {
     $window.njDrawChart();
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            windowClass: 'xx-dialog',
+            templateUrl: 'views/intro_popup.html',
+            controller: 'IntroModalCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.toggleAnimation = function () {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+
+    $scope.open('sm');
+
 }]);
+
+angular.module('futuresApp').controller('IntroModalCtrl', function ($scope, $modalInstance, items) {
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
