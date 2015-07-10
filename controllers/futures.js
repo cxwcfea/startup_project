6fcfcfe70788286e78c09331c75afc19cfcb8f05
cxwcfea <cxwcfea@163.com>
2015-07-10 14:28:00 +0800

    ppj continue

--- controllers/futures.js
+++ controllers/futures.js
@@ -15,12 +15,17 @@ var User = require('../models/User'),
 module.exports = {
     registerRoutes: function(app, passportConf) {
         app.get('/futures', function(req, res, next) {
+            /*
             util.getUserViewModel(req.user, function(user) {
                 console.log(user);
                 res.render('futures/index', {
                     layout:null,
                     bootstrappedUserObject: JSON.stringify(user)
                 });
+            });
+            */
+            res.render('futures/index', {
+                layout:null
             });
         });
 
