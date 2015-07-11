adb5aed02d52d8f6cae4f1fadecb68ff19006627
cxwcfea <cxwcfea@163.com>
2015-07-11 21:33:22 +0800

    ppj continue

--- public/futures/js/controller/FuturesOrdersCtrl.js
+++ public/futures/js/controller/FuturesOrdersCtrl.js
@@ -1,8 +1,4 @@
 'use strict';
 angular.module('futuresApp').controller('FuturesOrdersCtrl', ['$scope', '$window', function($scope, $window) {
-    //$scope.user = $scope.data.currentUser;
-    $scope.user = {
-        wechat_img: 'http://wx.qlogo.cn/mmopen/uchmtWQh7iarnsx58BbzlB1GAOzjI3S8elKicd6t8CiahGE3JSDmkVnTNzicQA44DdRliaLOsI1wLI9o79kynDUIchg/0',
-        wechat_name: '程翔'
-    }
+    $scope.user = $scope.data.currentUser;
 }]);
