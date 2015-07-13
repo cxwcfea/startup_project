6dc18f0a631425532210706cec74d69b3cbd02ab
cxwcfea <cxwcfea@163.com>
2015-07-13 14:36:30 +0800

    ppj continue

--- public/futures/js/controller/FuturesOrdersCtrl.js
+++ public/futures/js/controller/FuturesOrdersCtrl.js
@@ -4,11 +4,14 @@ angular.module('futuresApp').controller('FuturesOrdersCtrl', ['$scope', '$window
 
     $http.get('/api/futures/get_orders')
         .success(function(data, status) {
+            /*
             var str = '';
             for (var key in data[0]) {
                 str += 'key:' + key + ' value:' + data[0][key];
             }
             alert(str);
+            */
+            $scope.orders = data;
         })
         .error(function(data, status) {
 
