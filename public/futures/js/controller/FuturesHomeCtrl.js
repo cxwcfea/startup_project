064d76656c1ef0e65728e0627e005c4a7ec1cf89
cxwcfea <cxwcfea@163.com>
2015-07-13 21:30:05 +0800

    ppj continue

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -18,7 +18,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
                 if (position) {
                     $scope.tradeData.up = position.longQuantity / HAND;
                     $scope.tradeData.down = position.shortQuantity / HAND;
-                    $scope.tradeData.sell = position.quantity / HAND;
+                    $scope.tradeData.sell = Math.abs($scope.tradeData.up - $scope.tradeData.down);
                 }
                 $scope.profit = data.user.cash / 100 - 1000000;
             })
@@ -145,7 +145,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
         $http.post('/api/futures/create_order', {quantity:quantity, force_close:forceClose})
             .success(function(data, status) {
                 getUserPositions();
-                if (type === 0) {
+                if ($scope.tradeData.sell === 0) {
                     $scope.openGainPopup('lg');
                 }
             })
