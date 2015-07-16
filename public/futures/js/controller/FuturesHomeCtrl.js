e47d85cee8bc16bfafc933f9ff8f7471adb11ed8
cxwcfea <cxwcfea@163.com>
2015-07-16 19:31:56 +0800

    ppj continue

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -187,7 +187,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
             .success(function(data, status) {
                 getUserPositions();
                 var type = data.quantity > 0 ? '涨' : '跌';
-                displayError('您成功买' + type + '1手,价格' + (data.price/100).toFixed(1));
+                displayError('您成功买' + type + '1手,价格' + (data.price/100).toFixed(1) + '元');
             })
             .error(function(data, status) {
                 displayError(data.error_msg);
