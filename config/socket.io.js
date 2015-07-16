07010911312734e8a9a27d7dee43b81fb0999ca0
cxwcfea <cxwcfea@163.com>
2015-07-16 12:10:59 +0800

    ppj continue

--- config/socket.io.js
+++ config/socket.io.js
@@ -5,6 +5,7 @@ var util = require('../lib/util'),
 var channelName = 'futures';
 //var users
 
+/*
 function generateInitData(cb) {
     global.redis_client.lrange('mt://future/IFHIST', 0, 999, function(err, data) {
         if (err) {
@@ -21,7 +22,6 @@ function generateInitData(cb) {
         cb(ret);
     });
 
-    /*
     var data = [], time = (new Date()).getTime(), i;
 
     for (i = -999; i <= 0; i += 1) {
@@ -31,32 +31,47 @@ function generateInitData(cb) {
         ]);
     }
     return data;
-    */
 }
+     */
+
+var historyData = [];
 
 // Define the Socket.io configuration method
 module.exports = function(io) {
-    io.on('connection', function(socket) {
-        console.log(socket.id + ' connected');
-        socket.on('join', function (name) {
-            console.log(name + ' joined');
-            generateInitData(function(data) {
-                socket.emit('history_data', data);
+    global.redis_client.lrange('mt://future/IFHIST', 0, 999, function(err, data) {
+        if (err) {
+            console.log(err.toString());
+        }
+        for (var i in data) {
+            var line = data[i];
+            line = JSON.parse(line.replace(/'/g, ''))[0];
+            historyData.push([line.ts/1000, parseFloat(line.LastPrice)]);
+        }
+
+        io.on('connection', function(socket) {
+            console.log(socket.id + ' connected');
+            socket.on('join', function (name) {
+                console.log(name + ' joined');
+                generateInitData(function(data) {
+                    socket.emit('history_data', data);
+                });
             });
         });
+        setInterval(function() {
+            mockTrader.getLastFuturesPrice(function(err, data) {
+                if (err) {
+                    console.log('getLastFuturesPrice error:' + err.toString());
+                    var x = (new Date()).getTime(), // current time
+                        y = util.getRandomInt(3600, 4000);
+                    historyData.push([x, y]);
+                    io.sockets.emit('new_data', [x, y]);
+                } else {
+                    var x = data.ts, // current time
+                        y = data.lastPrice;
+                    historyData.push([x, y]);
+                    io.sockets.emit('new_data', [x, y]);
+                }
+            });
+        }, 30000);
     });
-    setInterval(function() {
-        mockTrader.getLastFuturesPrice(function(err, data) {
-            if (err) {
-                console.log('getLastFuturesPrice error:' + err.toString());
-                var x = (new Date()).getTime(), // current time
-                    y = util.getRandomInt(3600, 4000);
-                io.sockets.emit('new_data', [x, y]);
-            } else {
-                var x = data.ts, // current time
-                    y = data.lastPrice;
-                io.sockets.emit('new_data', [x, y]);
-            }
-        });
-    }, 30000);
 };
