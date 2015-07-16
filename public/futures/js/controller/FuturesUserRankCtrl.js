d085c6d2af46eba3a6d059da1e7e17e2ba9f14b1
cxwcfea <cxwcfea@163.com>
2015-07-16 16:49:45 +0800

    ppj continue

--- public/futures/js/controller/FuturesUserRankCtrl.js
+++ public/futures/js/controller/FuturesUserRankCtrl.js
@@ -2,7 +2,7 @@
 angular.module('futuresApp').controller('FuturesUserRankCtrl', ['$scope', '$window', '$location', '$http', '$filter', function($scope, $window, $location, $http, filter) {
     $http.get('/api/futures/user_rank')
         .success(function(data, status) {
-            var users = $filter('orderBy')(data.users, 'wechat.trader.cash', false);
+            var users = $filter('orderBy')(data.users, 'wechat.trader.cash', true);
             $scope.goldUser = users.shift();
             if (!$scope.goldUser) {
                 $scope.goldUser = {};
