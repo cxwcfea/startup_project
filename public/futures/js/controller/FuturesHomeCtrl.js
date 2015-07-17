d7e2cbbbbda158ecfb15ccdb9276822b00e3e01a
cxwcfea <cxwcfea@163.com>
2015-07-17 17:56:30 +0800

    ppj continue

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -208,7 +208,12 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
                 getUserPositions();
                 var orderType = data.quantity > 0 ? '涨' : '跌';
 
-                var newData = [data.timestamp, data.price/100];
+                var newData = {
+                    x: data.timestamp,
+                    y: data.price/100,
+                    text: (data.price/100).toFixed(0),
+                    title: (data.price/100).toFixed(0)
+                };
                 $window.niujin_chart_flags.addPoint(newData, true, true);
 
                 if (type != 0) {
