d7fc359c03d875324c6713a4149810e6ccc2933d
cxwcfea <cxwcfea@163.com>
2015-07-25 15:51:45 +0800

    ppj continue

--- config/socket.io.js
+++ config/socket.io.js
@@ -38,8 +38,8 @@ function fetchHistoryData(cb) {
         }
         cb(ret);
     });
-    for (var i = 1; i < products.length; ++i) {
-        global.redis_client.lrange(products[i].historyKey, 0, -1, function(err, data) {
+    for (var j = 1; j < products.length; ++j) {
+        global.redis_client.lrange(products[j].historyKey, 0, -1, function(err, data) {
             if (err) {
                 console.log(err.toString());
             }
@@ -49,9 +49,9 @@ function fetchHistoryData(cb) {
                 line = JSON.parse(line.replace(/'/g, ''))[0];
                 ret.unshift([parseInt(line.ts/1000), parseInt(line.LastPrice)]);
             }
-            products[i].historyData = ret;
-            console.log('got ' + products[i].name + ' data');
-            console.log(products[i].historyData);
+            products[j].historyData = ret;
+            console.log('got ' + products[j].name + ' data');
+            console.log(products[j].historyData);
             cb();
         });
     }
