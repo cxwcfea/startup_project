6024308de01eca7584b8722d0da8d56a60258725
shilin <shilin@niujinwang.com>
2015-07-26 16:22:29 +0800

    modify

--- public/futures/js/controller/FuturesOrdersCtrl.js
+++ public/futures/js/controller/FuturesOrdersCtrl.js
@@ -27,6 +27,7 @@ angular.module('futuresApp').controller('FuturesOrdersCtrl', ['$scope', '$window
 	
 	$scope.drawPecetageSector = function() {
 		$scope.drawSector(100,100,50,0,MATH.PI*1.5);
+		alert("hehe");
 	}
 	
 }]);
