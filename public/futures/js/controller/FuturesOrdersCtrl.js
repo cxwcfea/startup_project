028980c55beca667dd56f996e3bd2aba1ac419d3
shilin <shilin@niujinwang.com>
2015-07-27 12:32:55 +0800

    modify

--- public/futures/js/controller/FuturesOrdersCtrl.js
+++ public/futures/js/controller/FuturesOrdersCtrl.js
@@ -14,5 +14,6 @@ angular.module('futuresApp').controller('FuturesOrdersCtrl', ['$scope', '$window
         });
 	
 	$window.njPersonChart($scope.originCapital,$scope.userInfo.cash);
+	$window.njPersonChart(1000000,1000000);
 	
 }]);
