c33980403d00124d97cb2d320d663906dc1e0180
cxwcfea <cxwcfea@163.com>
2015-07-27 20:49:41 +0800

    ppj continue

--- public/futures/js/controller/FuturesOrdersCtrl.js
+++ public/futures/js/controller/FuturesOrdersCtrl.js
@@ -4,18 +4,18 @@ angular.module('futuresApp').controller('FuturesOrdersCtrl', ['$scope', '$window
     $scope.data.selectedItem = 3;
     $scope.originCapital = 1000000;
 
-    $http.get('/api/futures/get_orders')
-        .success(function(data, status) {
-            $scope.orders = data.orders;
-            $scope.userInfo = data.user;
-            alert(data.pageCount);
-			//$window.njPersonChart($scope.originCapital.toFixed(2),($scope.userInfo.cash/100).toFixed(2));
-			$window.njPersonChart($scope.originCapital, ($scope.userInfo.cash/100));
-        })
-        .error(function(data, status) {
-            alert('error');
-        });
-	
-	//$window.njPersonChart($scope.originCapital,$scope.userInfo.cash / 100);
+    function getOrderForPage(pageNum) {
+        $http.get('/api/futures/get_orders?page=' + pageNum)
+            .success(function(data, status) {
+                $scope.orders = data.orders;
+                $scope.userInfo = data.user;
+                $window.njPersonChart($scope.originCapital, ($scope.userInfo.cash/100));
+            })
+            .error(function(data, status) {
+                alert('error');
+            });
+    }
+
+    getOrderForPage(1);
 
 }]);
