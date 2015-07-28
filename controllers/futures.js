384f86afe5ea04e23a01e545c102ba2797fe5395
cxwcfea <cxwcfea@163.com>
2015-07-28 19:54:48 +0800

    ppj continue

--- controllers/futures.js
+++ controllers/futures.js
@@ -249,7 +249,7 @@ module.exports = {
 
             res.render('futures/' + req.params[0], {
                 layout:null,
-                tradeTime: true
+                tradeTime: tradeTime
             });
         });
     }
