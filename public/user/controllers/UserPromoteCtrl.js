5ab6acd9fb15a90ad8aa47d849c6cfbf1dc6869c
cxwcfea <cxwcfea@163.com>
2015-07-30 11:49:38 +0800

    release commission trans restriction

--- public/user/controllers/UserPromoteCtrl.js
+++ public/user/controllers/UserPromoteCtrl.js
@@ -8,7 +8,7 @@ angular.module('userApp').controller('UserPromoteCtrl', ['$scope', '$window', '$
 
     vm.total_fee = vm.user.finance.history_commission ? vm.user.finance.history_commission / 0.07 : 0;
     vm.paid_fee = vm.user.finance.history_commission ? vm.user.finance.history_commission - vm.user.finance.commission : 0;
-    vm.transAmount = vm.user.finance.commission ? Math.floor(vm.user.finance.commission / 100) * 100 : 0;
+    vm.transAmount = vm.user.finance.commission ? Math.round(vm.user.finance.commission) : 0;
     if (vm.user.referName) {
         vm.referName = vm.user.referName.substr(2);
     } else {
