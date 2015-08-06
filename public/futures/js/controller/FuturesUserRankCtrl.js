d8ab68f6f890de2e8d46a77acc16efc5af97a517
cxwcfea <cxwcfea@163.com>
2015-08-06 10:53:45 +0800

    fix capital display bug

--- public/futures/js/controller/FuturesUserRankCtrl.js
+++ public/futures/js/controller/FuturesUserRankCtrl.js
@@ -2,7 +2,7 @@
 angular.module('futuresApp').controller('FuturesUserRankCtrl', ['$scope', '$window', '$location', '$http', '$filter', function($scope, $window, $location, $http, filter) {
     $scope.user = $scope.data.currentUser;
     $scope.data.selectedItem = 2;
-    $scope.originCapital = 150000;
+    $scope.originCapital = 15000000;
 
     $http.get('/api/futures/user_rank')
         .success(function(data, status) {
