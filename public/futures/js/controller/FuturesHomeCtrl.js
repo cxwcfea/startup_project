c45949761f84a4d15207c5b4b54363723b80128e
cxwcfea <cxwcfea@163.com>
2015-08-12 22:33:02 +0800

    enable trade in test

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -54,7 +54,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
             }
         }
     } else {
-        $scope.tradeClose = true;
+        //$scope.tradeClose = true;
     }
 
     if ($scope.data.real && $scope.user.wechat.status !== 4) {
