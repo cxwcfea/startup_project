a0416d91ccdf12e06ff7b28935316b18e52c48c6
cxwcfea <cxwcfea@163.com>
2015-08-12 15:04:44 +0800

    frequent change

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -126,7 +126,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
     fetchUserProfit();
     $interval(function() {
         fetchUserProfit();
-    }, 1000);
+    }, 500);
 
     $scope.showShareHint = false;
     $scope.openIntroPopup = function (size) {
