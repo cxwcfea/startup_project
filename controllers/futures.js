d3c1870acf1c7cf4f442e235b5426eab00d8f5c9
cxwcfea <cxwcfea@163.com>
2015-08-17 18:42:47 +0800

    admin page

--- controllers/futures.js
+++ controllers/futures.js
@@ -558,6 +558,7 @@ function getProfit(req, res) {
                     return res.status(500).send({error_msg:'找不到交易用户'});
                 }
                 var profit = (trader.cash - (trader.deposit + trader.debt)) / 100;
+                logger.debug('getProfit ', profit, profitAmount);
                 if (profitAmount > profit) {
                     return res.status(500).send({error_msg:'无效的盈利金额'});
                 }
