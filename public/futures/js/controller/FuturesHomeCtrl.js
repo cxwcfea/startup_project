47e3bbbd5f673d935030dc91b7fa28b2f0e3182b
cxwcfea <cxwcfea@163.com>
2015-08-17 19:03:43 +0800

    for test

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -63,7 +63,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
             }
         }
     } else {
-        $scope.tradeClose = true;
+        //$scope.tradeClose = true;
     }
 
     if ($scope.data.real && $scope.user.wechat.status !== 4) {
