03b06be39e4fe13742bc027efa9521cea54adecb
cxwcfea <cxwcfea@163.com>
2015-08-17 18:51:34 +0800

    for production

--- controllers/futures.js
+++ controllers/futures.js
@@ -887,7 +887,7 @@ module.exports = {
 
             res.render('futures/' + req.params[0], {
                 layout:null,
-                tradeTime: true
+                tradeTime: tradeTime
             });
         });
     },
