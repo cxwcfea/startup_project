f37a532dd68117d1e2e4741cb84db35e159f63bc
cxwcfea <cxwcfea@163.com>
2015-08-25 15:40:16 +0800

    get nearest order

--- public/futures/js/controller/FuturesIndexCtrl.js
+++ public/futures/js/controller/FuturesIndexCtrl.js
@@ -25,7 +25,8 @@ angular.module('futuresApp').controller('FuturesIndexCtrl', ['$scope', '$window'
     };
     */
 
-	$scope.data.status = 0;
+    $scope.data.flags_data = [];
+    $scope.data.status = 0;
 	
     $scope.data.productID = 0;
     $scope.data.selectedItem = 1;
