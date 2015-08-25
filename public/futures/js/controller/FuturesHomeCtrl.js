ee9cda11eb1a716f775fc3a2b44cc9755df17196
cxwcfea <cxwcfea@163.com>
2015-08-25 20:44:24 +0800

    add log when place order

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -96,7 +96,7 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
             .error(function(data, status) {
                 console.log(data.error_msg);
             });
-    }, 1000);
+    }, 2000);
 
 
     $scope.tradeData = {
