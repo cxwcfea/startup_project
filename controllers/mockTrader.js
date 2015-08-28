1fc074848ff19f43b004258af262c03f443daa02
cxwcfea <cxwcfea@163.com>
2015-08-28 11:46:11 +0800

    fix bug

--- controllers/mockTrader.js
+++ controllers/mockTrader.js
@@ -769,6 +769,7 @@ function getLastFuturesPrice(contract, cb) {
         }
         var priceInfo = JSON.parse(priceInfoString);
         priceInfo.LastPrice *= 100;
+        console.log('getLastFuturesPrice', priceInfo);
         cb(null, {ts:priceInfo.ts, lastPrice:priceInfo.LastPrice, yesterdayClose:priceInfo.PreSettlementPrice});
     });
 }
