770260b21b1282a4b32500c4751441c8cb875bd0
cxwcfea <cxwcfea@163.com>
2015-08-31 14:16:51 +0800

    for debug

--- public/futures/js/controller/FuturesHomeCtrl.js
+++ public/futures/js/controller/FuturesHomeCtrl.js
@@ -69,6 +69,13 @@ angular.module('futuresApp').controller('FuturesHomeCtrl', ['$scope', '$window',
     thirdStart.minute(57);
     thirdStart.second(0);
 
+    console.log('now ' + now.toDate());
+    console.log('firstStart ' + firstStart.toDate());
+    console.log('firstEnd ' + firstEnd.toDate());
+    console.log('secondStart ' + secondStart.toDate());
+    console.log('secondEnd ' + secondEnd.toDate());
+    console.log('thirdStart ' + thirdStart.toDate());
+    console.log('thirdEnd ' + thirdEnd.toDate());
     if ((now >= firstStart && now <= firstEnd) || (now >= secondStart && now <= secondEnd) || (now >= thirdStart && now <= thirdEnd)) {
         if (!$scope.data.timeoutSet) {
             $scope.data.timeoutSet = true;
